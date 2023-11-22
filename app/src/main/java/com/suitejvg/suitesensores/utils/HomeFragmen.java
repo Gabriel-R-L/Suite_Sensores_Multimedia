fbf9afdfb62b3b4d0122eae77a5da2898078a287
Vix <you@example.com>
2023-11-22 10:49:57 +0100

    a

--- app/src/main/java/com/suitejvg/suitesensores/utils/HomeFragmen.java
+++ app/src/main/java/com/suitejvg/suitesensores/utils/HomeFragmen.java
@@ -5,12 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.suitejvg.suitesensores.R;
-
 
 public class HomeFragmen extends Fragment {
 
